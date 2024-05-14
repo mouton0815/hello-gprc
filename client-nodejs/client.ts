e022f32cbaac8930cf61657f1d5c192a42fbf498
Torsten Schlieder <torsten.schlieder@unite.eu>
2024-05-14 19:05:02 +0200

    Tidier client code

--- client-nodejs/client.ts
+++ client-nodejs/client.ts
@@ -1,7 +1,7 @@
-import * as grpc from '@grpc/grpc-js'
+import { credentials } from '@grpc/grpc-js'
 import { GreeterClient, HelloRequest } from './proto/hello'
 
-const client = new GreeterClient('localhost:5005', grpc.credentials.createInsecure())
+const client = new GreeterClient('localhost:5005', credentials.createInsecure())
 const request = HelloRequest.create({ name: 'Hans' })
 client.sayHello(request, (error, response) => {
     console.log(error ? error : response.message)
