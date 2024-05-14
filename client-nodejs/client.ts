52279ebb454c4c99cbd7121bc4aed71d70347f56
Torsten Schlieder <torsten.schlieder@unite.eu>
2024-05-14 12:16:47 +0200

    Simplified client

--- client-nodejs/client.ts
+++ client-nodejs/client.ts
@@ -2,12 +2,7 @@ import * as grpc from '@grpc/grpc-js'
 import { GreeterClient, HelloRequest } from './proto/hello'
 
 const client = new GreeterClient('localhost:5005', grpc.credentials.createInsecure())
-const request = HelloRequest.create()
-request.name = 'Hans'
+const request = HelloRequest.create({ name: 'Hans' })
 client.sayHello(request, (error, response) => {
-    if (error) {
-        console.error(error);
-    } else {
-        console.log(response.message);
-    }
+    console.log(error ? error : response.message)
 })
