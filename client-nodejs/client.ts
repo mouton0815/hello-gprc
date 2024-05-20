20b3e35b375bd4ff32bc4bf7cf752fc6fcd677a0
Torsten Schlieder <torsten.schlieder@unite.eu>
2024-05-20 11:10:14 +0200

    Remove Node.js (is now in new project)

--- client-nodejs/client.ts
+++
@@ -1,8 +0,0 @@
-import { credentials } from '@grpc/grpc-js'
-import { GreeterClient, HelloRequest } from './proto/hello'
-
-const client = new GreeterClient('localhost:5005', credentials.createInsecure())
-const request = HelloRequest.create({ name: 'Hans' })
-client.sayHello(request, (error, response) => {
-    console.log(error ? error : response.message)
-})
